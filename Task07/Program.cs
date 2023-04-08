a134507bcbf4f66b08096b836ed30788e6694dbe
AVA <Marsota_mixer@tut.by>
2023-04-08 23:51:21 +0300

    modify task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -5,12 +5,12 @@
 // 782 -> 2
 // 918 -> 8
 
-Console.Write ("Введите число: ");
+Console.Write ("Введите трехзначное число: ");
 int number = Convert.ToInt32 (Console.ReadLine());
 if (number < 0)
     number = -number;
 if (number >= 100 && number <= 999)
-Console.WriteLine (number%10);
+Console.WriteLine ($"последння цифра в этом числе: {number%10}");
 else
 {
     Console.WriteLine ("Вы ввели не трехзначное значное число");
