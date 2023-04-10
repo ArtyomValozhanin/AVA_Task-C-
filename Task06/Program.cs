58c4466cab6a1be039f3daa6370e29c527d00051
AVA <Marsota_mixer@tut.by>
2023-04-10 15:20:43 +0300

    modify Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -10,9 +10,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number%2 == 0)
 {
-    Console.WriteLine($"Число: {number} является четным");
+    Console.WriteLine($"Число: {number} является чётным");
 }
 else
 {
-    Console.WriteLine($"Число: {number} нечетное");
+    Console.WriteLine($"Число: {number} не является чётным");
 }
