57b941ff06cbf1801491c0b3f16edf55eb0d6f79
AVA <Marsota_mixer@tut.by>
2023-04-10 15:22:17 +0300

    modify Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number < 0)
     number = -number;
-int count = 0+2;
+int count = + 2;
 while (count <= number)
 {
     Console.Write($"{count},");
