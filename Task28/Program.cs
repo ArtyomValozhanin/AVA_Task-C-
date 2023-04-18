8e8a6dea1ff8e44d77c36807a05df6647f9b8a1d
AVA <Marsota_mixer@tut.by>
2023-04-18 14:21:46 +0300

    added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumbersMultiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+int NumbersMultiplication(int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            res *= i; // res = res * i;
+        }
+    }
+    return res;
+}
