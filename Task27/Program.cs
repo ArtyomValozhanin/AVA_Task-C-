0c15a2d263858c8530b8072a06b1a88eae4d24b5
AVA <Marsota_mixer@tut.by>
2023-04-19 17:39:10 +0300

    added Task27 SumDigit

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigit(number);
+Console.WriteLine($"Сумма чисел в числе {number} = {result}");
+
+int SumDigit(int num)
+{
+    int count = 0;
+   while (num > 1)
+    {
+        count = count + num % 10 - 1;
+        num = num / 10;             
+        count++; 
+    }
+    return count;
+}
