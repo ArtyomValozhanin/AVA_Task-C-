9283957337e2d02449c5402e22b195e426f2d205
AVA <Marsota_mixer@tut.by>
2023-04-19 17:38:14 +0300

    added Task25 powNumber

---
+++ Task25/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите первое число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = PowNumbers(numberA, numberB);
+Console.WriteLine($"{numberA} ^{numberB} -> {result}");
+
+int PowNumbers(int numA, int numB) // через цикл "for"
+{
+    int powNum = 0;
+    int temp = numA;
+    for (int i = 1; i <= numB - 1; i++)
+    {
+        checked
+        {
+            powNum = temp * numA;
+            temp = powNum;
+        }
+    }
+    return powNum;
+}
+// int PowNumbers(int numA, int numB) // через цикл "while"
+// {
+//     int powNum = 0;
+//     int temp = numA;
+//     int count = 1;
+//     while (count <= numB - 1)
+//     {
+//         checked // Проверка на переполнение
+//         {
+//             powNum = temp * numA;
+//             temp = powNum;
+//             count ++;
+//         }
+//     }
+//     return powNum;
+// }
