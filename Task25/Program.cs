6e2b954cd598bf5784447273bb5431dbf2516db2
AVA <Marsota_mixer@tut.by>
2023-04-21 13:07:40 +0300

    modify Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -9,7 +9,7 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberB < 0) numberB = -numberB;
+if (numberB < 0) numberB = -numberB; // <- сообщить что ввод с "-" не верный
 int result = PowNumbers(numberA, numberB);
 Console.WriteLine($"{numberA} ^{numberB} -> {result}");
 
@@ -17,7 +17,7 @@ int PowNumbers(int numA, int numB) // через цикл "for"
 {
     int powNum = 0;
         int temp = numA;
-    for (int i = 1; i <= numB - 1; i++)
+    for (int i = 1; i < numB; i++)
     {
         checked
         {
