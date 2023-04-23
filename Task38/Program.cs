b6bbaf1d97d95b41fa7cb1c24cbb2d5b170eee96
AVA <Marsota_mixer@tut.by>
2023-04-23 10:33:53 +0300

    modify Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -15,44 +15,63 @@ double[] CreateArrayRndDouble(int size, int min, int max) // создание м
     return arr;
 }
 
-void PrintArray (double[] arr) // функция вывода массива на печать.
+void PrintArray(double[] arr) // функция вывода массива на печать.
 {
     for (int i = 0; i < arr.Length; i++)
     {
         double round = Math.Round(arr[i], 1); // округдение до одного знака
-        if(i < arr.Length - 1) Console.Write($"{round}, ");
+        if (i < arr.Length - 1) Console.Write($"{round}, ");
         else Console.Write($"{round}");
     }
 }
 
-double FindMinElement (double[] arr)
+double FindDiffMinMaxElement(double[] arr)
 {
+    double diffMaxMin = 0;
     double min = arr[0];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        double round = Math.Round(arr[i], 1);
-        if(round < min) min = round; 
-    }
-    return min;
-}
-
-double FindMaxElement (double[] arr)
-{
     double max = arr[0];
     for (int i = 0; i < arr.Length; i++)
     {
         double round = Math.Round(arr[i], 1);
-        if(round > max) max = round; 
+        if (round > max) max = round;
+        if (round < min) min = round;
+        diffMaxMin = max - min;
     }
-    return max;
+    return diffMaxMin;
 }
+
+// Решение отдельными методами:
+// double FindMinElement (double[] arr)
+// {
+//     double min = arr[0];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         double round = Math.Round(arr[i], 1);
+//         if(round < min) min = round; 
+//     }
+//     return min;
+// }
+
+// double FindMaxElement (double[] arr)
+// {
+//     double max = arr[0];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         double round = Math.Round(arr[i], 1);
+//         if(round > max) max = round; 
+//     }
+//     return max;
+// }
 
 double[] array = CreateArrayRndDouble(6, -2, 20);
 Console.Write("[");
 PrintArray(array);
 Console.Write("]");
 Console.WriteLine();
-double resultMax = FindMaxElement(array);
-double resultMin = FindMinElement(array);
-double diffresult = FindMaxElement(array) - FindMinElement(array);
-Console.WriteLine($"Разница между значениями {resultMax} и {resultMin} составляет: {Math.Round(diffresult, 1)}");
+double diffresult = FindDiffMinMaxElement(array);
+Console.WriteLine($"Разница между Max и Min значениями массива = {diffresult}"); 
+
+// double resultMax = FindMaxElement(array);
+// double resultMin = FindMinElement(array);
+// double diffresult = FindMaxElement(array) - FindMinElement(array);
+// Console.WriteLine($"Разница между значениями {resultMax} и {resultMin} составляет: {Math.Round(diffresult, 1)}");
