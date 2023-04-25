2554380d57d82debb5f79b5d8883df9c4fa751b9
AVA <Marsota_mixer@tut.by>
2023-04-25 16:52:13 +0300

    rename Task40-45

---
+++ Task44_Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// Цифры фибоначи
+// 1 1 2 3 5 8 13 21
+// f[n] = f[n-1] + f[n-2]
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] result = Fibonacci(number);
+PrintArray(result);
+
+int[] Fibonacci(int num)
+{
+    int[] array = new int[num];
+    //    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
---
+++ Task45_CopyArray/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+
+void CreateArray(int[] array)
+{
+    Random random = new Random(); // оптимизация объекта Random
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(10, 99);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+    for (int i = 0; i < length; i++)
+    {
+         newArr[i] = arr[i];
+    }
+    return newArray;
+}
+
+int[] array = CreateArrayRndInt(5, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+int[] newArray=CreateCopyArray(array);
+// newArray[0] = 10;
+PrintArray(newArray);
